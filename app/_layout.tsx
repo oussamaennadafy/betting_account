69b20fe1649c07a72f574034ce2c37b6559f61c3
sam <sam@ora.ma>
2024-05-26 14:16:52 +0100

    first commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout() {
   if (!loaded) {
     return null;
   }
-
+  
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
